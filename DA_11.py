c2313608ef8240c5c27e20fcdb242645cb8564f8
darylliew <liew_chow_yee_daryl@connect.ite.edu.sg>
2021-08-27 12:08:52 +0800

    Newly updated code

--- DA_11.py
+++ DA_11.py
@@ -2,7 +2,7 @@ import requests
 
 url = "http://brickset.com/sets/year-2008"
 r = requests.get(url)
-# This will get the full page
+
 print(r.text)
 print("Status code:")
 print("\t *", r.status_code)
@@ -10,7 +10,7 @@ print("\t *", r.status_code)
 h = requests.head(url)
 print("Header:")
 print("**********")
-# To print line by line
+
 
 for x in h.headers:
     print("\t ", x, ":", h.headers[x])
@@ -19,7 +19,7 @@ print("**********")
 headers = {
     'User-Agent' : "Mobile"
 }
-# Test it on an external site
+
 url2 = 'http://httpbin.org/headers'
 rh = requests.get(url2, headers=headers)
 print(rh.text)
@@ -44,7 +44,35 @@ class BrickSetSpider(scrapy.Spider):
                 response.urljoin(next_page),
                 callback=self.parse
             )
+import unittest
+class TestScrapper(unittest.TestCase):
+    def test_requests(self):
+        print("Spoofing current header....\n")
+        headers = {'User-Agent': "Mobile"}
+        for url in self.start_urls:
+            yield requests(url, headers=headers)
+        url2 = 'http://httpbin.org/headers'
+        rh = requests.get(url2, headers=headers)
+        print(rh.text)
 
+    def parse(self, response):
+        SET_SELECTOR = '.set'
+        for brickset in response.css(SET_SELECTOR):
+            IMAGE_SELECTOR = 'img ::attr(src)'
+            yield {
+                'image': brickset.css(IMAGE_SELECTOR).extract_first(),
+            }
+
+        NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
+        next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
+        if next_page:
+            yield scrapy.Request(
+                response.urljoin(next_page),
+                callback=self.parse
+            )
+
+if __name__=='_main_':
+    unittest.main
 
 
 
